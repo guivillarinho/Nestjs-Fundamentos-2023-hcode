d21e49990eff7eda3d54244e5943b742964d20bd
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-06-20 14:09:14 -0300

    feat: (user.module.ts) :sparkles: Added PrismaModule and UserService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [UserController],
-  providers: [],
+  providers: [UserService],
   exports: [],
 })
 export class UserModule {}
