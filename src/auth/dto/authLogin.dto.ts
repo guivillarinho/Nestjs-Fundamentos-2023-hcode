5c5b4827580b31dda9ee8b3dab2a18225c4ca271
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-06-26 15:20:42 -0300

    feat: :sparkles: Add auth Login dto

---
+++ src/auth/dto/authLogin.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsStrongPassword } from "class-validator"
+
+export class AuthLoginDto {
+    @IsEmail()
+    email: string
+
+    @IsStrongPassword({
+        minLength: 6,
+        minUppercase: 1,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    })
+    password: string
+}
