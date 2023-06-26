542303dfb76b89fcef501ff482bb6b9b3c707509
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-06-26 15:15:59 -0300

    feat: (user.middleware.ts) :sparkles: Adding Injectable decorator

--- src/user/middleware/user.middleware.ts
+++ src/user/middleware/user.middleware.ts
@@ -1,12 +1,11 @@
-import { BadRequestException,  NestMiddleware } from "@nestjs/common";
+import { BadRequestException,  Injectable,  NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
-
+@Injectable()
 export class UserIDlMiddlewareVerify implements NestMiddleware {
     async use(req: Request, _: Response, next: NextFunction) {
        if(isNaN(Number(req.params.id)) || Number(req.params.id) <= 0){
-        throw new BadRequestException('Id inválido!')
+            throw new BadRequestException('Id inválido!')
        }
-
        next()
     }
 }
