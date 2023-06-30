ec13f402748957a8fe7056722629c88e06777d66
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-06-30 10:01:10 -0300

    feat: (auth.guard.ts) :sparkles: Add some exemplifications

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -2,17 +2,25 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthService } from "src/auth/auth.service";
 
 @Injectable()
+/**
+ * Guarda de verificação de autenticação JWT válido
+ */
 export class AuthGuard implements CanActivate {
 
     constructor(private readonly authService: AuthService){}
 
     canActivate(context: ExecutionContext) {
+
+        //Pegando o request da requisição e depois é pego somente o campo de authorization que vem do request.headers ("Cabeçalhos da requisição")
         const request = context.switchToHttp().getRequest();
         const {authorization} = request.headers
+
+        //Após pegar o authorization, é feito uma checagem do token recebido. Com isso, passamos esse valor para uma parte personalizada chamada reques.tokenPayload. E por fim retornamos true para permitir acesso na rota. 
         try{         
-            const data = this.authService.checkToken((authorization ?? '').split(' ')[1])
+            const data = this.authService.checkToken((authorization ?? '').split(' ')[1])//Pegando somente a parte 1 do array com o token, para aí sim retornar o payload, e fazendo o check do token. 
+
             request.tokenPayload = data
-            console.log(data)
+
             return true
         } catch(error){
             return false 
