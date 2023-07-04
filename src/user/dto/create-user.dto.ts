f09674a8136cf68a28ca3bfbc3bd1c7021e6e050
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-04 14:00:47 -0300

    feat(create-user.dto.ts): :sparkles: Adding role
    Role is a enum and optional

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,5 @@
-import { IsDateString, IsEmail, IsOptional, IsString, IsStrongPassword } from 'class-validator'
+import { IsDateString, IsEmail, IsEnum, IsOptional, IsString, IsStrongPassword } from 'class-validator'
+import { Role } from 'src/enums/role.enum'
 
 export class CreateUserDto {
     @IsString()
@@ -20,4 +21,8 @@ export class CreateUserDto {
     @IsOptional()
     @IsDateString()
     birthAt?: Date 
+
+    @IsOptional()
+    @IsEnum(Role)
+    role: string
 }
