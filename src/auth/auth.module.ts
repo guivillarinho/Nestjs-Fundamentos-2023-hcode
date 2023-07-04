d3bd113ba2570238b3ce23ca98217949efd4d40c
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-04 14:39:59 -0300

    refactor: :dizzy: Removing circular dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,22 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UserModule } from "src/user/user.module";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthGuard } from "src/guards/auth.guard";
 
 @Module({
     imports: [
         JwtModule.register({
             secret: 'l8S6$85s62c934J@3AYl&c43esYOr4cG'
         }),
-        UserModule, 
+        forwardRef(() => UserModule), 
         PrismaModule
     ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService],
+    exports: [AuthService]
 })
 export class AuthModule {
 
