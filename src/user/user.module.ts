3accbb5add5cd32b73e12c80857b7426b602825b
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-04 14:02:56 -0300

    refactor(user.module.ts): :dizzy: Remove circular dependencies

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod, forwardRef } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserService } from './user.service';
 import { UserIDlMiddlewareVerify } from './middleware/user.middleware';
-
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
