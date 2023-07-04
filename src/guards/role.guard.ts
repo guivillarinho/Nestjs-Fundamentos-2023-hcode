0ac24d77b53451c05363f81d1315ba29a87091c2
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-04 14:36:38 -0300

    style(role.guard.ts): :page_facing_up: Removing defined but never user declarations

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,6 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
-import { User } from "@prisma/client";
 import { ROLE_KEYS } from "src/decorators/role.decorator";
 import { Role } from "src/enums/role.enum";
 
@@ -15,11 +14,11 @@ export class RoleGuard implements CanActivate{
         if(!requiredRoles){
             return true
         }
-        
+
         const {user} = context.switchToHttp().getRequest()
 
         if(user.role !== Role.Admin){
-            throw new UnauthorizedException('Usuário não validado')
+            return false
         }
 
         return true
