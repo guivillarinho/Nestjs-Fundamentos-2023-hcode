80a2b175c007f64ede4b04329520b59250381d58
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-05 10:19:44 -0300

    feat(app.module.ts): :sparkles: Adding throttler config and guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,27 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
+import {ThrottlerGuard, ThrottlerModule} from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core';
+
 @Module({
-  imports: [forwardRef(() => UserModule), PrismaModule, forwardRef(() => AuthModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    PrismaModule,
+    forwardRef(() => AuthModule),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 20
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    ({
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    })
+  ],
   exports: [AppService]
 })
 export class AppModule {}
