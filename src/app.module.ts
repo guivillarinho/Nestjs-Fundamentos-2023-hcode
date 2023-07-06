ce5b113ab9271a2133e959b1baa178e19f938030
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-06 15:26:40 -0300

    feat(app.module.ts): :sparkles: Adding MailerModule at app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import {ThrottlerGuard, ThrottlerModule} from '@nestjs/throttler'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core';
 
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 @Module({
   imports: [
     forwardRef(() => UserModule),
@@ -18,7 +20,20 @@ import { APP_GUARD } from '@nestjs/core';
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 20
-    })
+    }),
+    MailerModule.forRoot({
+      transport: 'smtps://faustino.towne@ethereal.email:4YGjf6YzxjrW4VcDEa@smtp.ethereal.email',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
