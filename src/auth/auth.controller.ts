b3c5f84ea408bcba7d8c985e1541479e12914e1c
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-06 07:34:54 -0300

    feat(auth.controller.ts): :sparkles: File upload config using fileservice

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthLoginDto } from "./dto/authLogin.dto";
 import { AuthRegisterDto } from "./dto/authRegister.dto";
@@ -7,11 +7,15 @@ import { AuthResetPasswordDto } from "./dto/authResetPassword.dto";
 import { AuthGuard } from "src/guards/auth.guard";
 import { UserDecorator } from "src/decorators/user.decorator";
 import { User } from "@prisma/client";
+import { FileInterceptor } from '@nestjs/platform-express'
+import { join } from "path";
+import { FileService } from "src/files/file.service";
 
 @Controller('auth')
 export class AuthController {
     constructor(
-        private readonly authService: AuthService
+        private readonly authService: AuthService,
+        private readonly fileService: FileService
     ){}
 
     @Post('login')
@@ -38,5 +42,21 @@ export class AuthController {
     @Post('reset')
     async resetPassword(@Body() {password, token}: AuthResetPasswordDto){
         return this.authService.resetPassword(password, token)
+    }
+
+    @UseInterceptors(FileInterceptor('file'))
+    @UseGuards(AuthGuard)
+    @Post('file')
+    async uploadFile(@UserDecorator() user: User, @UploadedFile() attachedFile: Express.Multer.File){
+         
+         const path = join(__dirname,'..', '..', 'storage', 'files', `file-${user.id}.png`)
+
+         try {
+            await this.fileService.upload(attachedFile, path)
+         } catch (error) {
+            throw new BadRequestException(error)
+         } 
+
+         return {success: true}
     }
 }
