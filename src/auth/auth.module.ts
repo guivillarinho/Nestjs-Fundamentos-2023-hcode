f8d19d58c9de6b3b366e7d912aabf5b7d012c95b
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-06 07:34:08 -0300

    feat(auth.module.ts): :sparkles: Importing FileModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UserModule } from "src/user/user.module";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { FileModule } from "src/files/file.module";
 
 @Module({
     imports: [
@@ -11,7 +12,8 @@ import { PrismaModule } from "src/prisma/prisma.module";
             secret: process.env.JWT_SECRET
         }),
         forwardRef(() => UserModule), 
-        PrismaModule
+        PrismaModule,
+        FileModule
     ],
     controllers: [AuthController],
     providers: [AuthService],
