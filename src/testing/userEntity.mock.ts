6a245662fc26db1fc8c8a9eb353f512010039574
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-12 11:35:21 -0300

    feat: (userEntity.mock.ts) :sparkles: Mocking userEntity

---
+++ src/testing/userEntity.mock.ts
@@ -0,0 +1,35 @@
+import { Role } from '../enums/role.enum';
+import { UserEntity } from '../user/entity/user.entity';
+
+export const userEntityList: UserEntity[] = [
+  {
+    name: 'Guilherme Villarinho',
+    email: 'guilherme.villarinho@cinbal.com.br',
+    birthAt: new Date('2023-03-02'),
+    id: 1,
+    password: '$2b$08$rr7HwHcVSEADRYS1T66W9u7RHuNQci9RDJdrBH.uLeu97.gfJ4GhG',
+    role: Role.Admin,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Arthur Oliveira',
+    email: 'arthur.oliveira@cinbal.com.br',
+    birthAt: new Date('2023-03-02'),
+    id: 2,
+    password: '$2b$08$rr7HwHcVSEADRYS1T66W9u7RHuNQci9RDJdrBH.uLeu97.gfJ4GhG',
+    role: Role.Admin,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Mateus Moura',
+    email: 'mateus.moura@cinbal.com.br',
+    birthAt: new Date('2023-03-02'),
+    id: 3,
+    password: '$2b$08$rr7HwHcVSEADRYS1T66W9u7RHuNQci9RDJdrBH.uLeu97.gfJ4GhG',
+    role: Role.Admin,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
