7c6db936c537c864138aa7908c6a602ec0b8ffe0
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-12 11:23:42 -0300

    feat: :sparkles: Dto mocks

---
+++ src/testing/userUpdateDto.mock.ts
@@ -0,0 +1,11 @@
+import { Role } from '../enums/role.enum';
+import { UpdateUserDtoTypePut } from '../user/dto/updateTypePut-user.dto';
+
+export const updateUserDtoMock: UpdateUserDtoTypePut = {
+  name: 'Guilherme Villarinho Oliveira',
+
+  email: 'guilherme.villarinho15@cinbal.com.br',
+  password: 'Vill@23*',
+  role: Role.Admin,
+  birthAt: new Date('2024-02-01'),
+};
