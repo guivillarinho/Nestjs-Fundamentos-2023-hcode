93b23c4c8fdf8b09572e0b5caa01ed03d8db3f1b
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-13 10:58:51 -0300

    feat: (auth.service.spec.ts) :sparkles: Starting the Authentication tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -7,6 +7,7 @@ import { userServiceMock } from '../testing/userService.mock';
 import { userEntityList } from '../testing/userEntity.mock';
 import { jwtAccessTokenMock } from '../testing/jwtAccessToken.mock';
 import { jwtPayloadMock } from '../testing/jwtPayload.mock';
+import { jwtResetTokenMock } from '../testing/jwtResetToken.mock';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -47,5 +48,32 @@ describe('AuthService', () => {
       expect(result).toEqual(true);
     });
   });
-  describe('Authentication', () => {});
+
+  describe('Authentication', () => {
+    test('login', async () => {
+      const result = await authService.login(
+        'guilherme.villarinho@gmail.com',
+        'Vill@23*',
+      );
+
+      expect(result).toEqual(jwtAccessTokenMock);
+    });
+
+    test('forgetPassword', async () => {
+      const result = await authService.forgetPassword(
+        'guilherme.villarinho@gmail.com',
+      );
+
+      expect(result).toEqual({ success: true });
+    });
+
+    test('resetPassword', async () => {
+      const result = await authService.resetPassword(
+        'Vill@24*',
+        jwtResetTokenMock,
+      );
+
+      expect(result).toEqual(jwtAccessTokenMock);
+    });
+  });
 });
