7007e87e094a6f7c959b9f24f9813e455d73775c
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-14 09:21:25 -0300

    feat(authuserregisterdto.mock.ts): :sparkles: Mocking authRegisterDto

---
+++ src/testing/authUserRegisterDto.mock.ts
@@ -0,0 +1,7 @@
+import { AuthRegisterDto } from '../auth/dto/authRegister.dto';
+
+export const authUserRegisterDtoMock: AuthRegisterDto = {
+  name: 'Guilherme Oliveira Villarinho',
+  email: 'guilherme.villarinho@cinbal.com.br',
+  password: 'Ex%mpl023*',
+};
